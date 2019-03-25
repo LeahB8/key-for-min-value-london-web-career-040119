97c32b481482ec0aa282f90a3def807e62c6a43f
Leah Boyd <leahboyd08@gmail.com>
2019-03-25 10:30:46 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,3 +7,17 @@ def key_for_min_value(name_hash)
  
   end
 end
+
+def key_for_min_value(name_hash)
+
+end 
+  min_value = 10000
+  min_key = nil
+  name_hash.each do |key, value|
+    if value<min_value
+      min_value=value
+      min_key=key
+  end
+end
+    return min_key
+end
